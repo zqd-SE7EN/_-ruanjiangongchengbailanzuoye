779989fa5b8bd53d6543ad27526c1811a195c89a
Se7en <89471139+zqd-SE7EN@users.noreply.github.com>
2022-05-15 12:21:15 +0800

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<style scoped="scoped">
+	#app{
+		width: 100vw;
+		height: 100vh;
+		background: linear-gradient(45deg,rgb(31, 0, 168),rgb(174, 0, 186));
+	}
+</style>
